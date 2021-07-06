b9842607d4fd9cadd7abfd093c3318b3f7dcff76
tykaind <cult40@naver.com>
2021-07-06 09:54:38 +0900

    실습

--- src/com/javaex/practice/Ex10.java
+++ src/com/javaex/practice/Ex10.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Ex10 {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		int j = 0;
 		System.out.println("숫자를 입력하세요.");
--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -7,7 +7,7 @@ public class Ex20 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-        int random = 40;
+        int random = (int)(Math.random()*100)+1;
         int i=0;
         
         System.out.println("===========================");
@@ -18,11 +18,11 @@ public class Ex20 {
         while(true) {
             System.out.print(">>");
             i = sc.nextInt();
-            if(i>random) {
+            if(i<random) {
             System.out.println("더 높게");
             continue;
         }else {
-        	if(i<random) {
+        	if(i>random) {
                 System.out.println("더 낮게");
                 continue;
         	}else {
@@ -35,11 +35,12 @@ public class Ex20 {
                         System.out.println("===========================");
                         System.out.println("   [숫자맞추기게임 시작]");
                         System.out.println("===========================");
+                        random = (int)(Math.random()*100)+1;
                     	continue;
                     }else {
-                    	System.out.println("=====================");
-                    	System.out.println("[숫자맞추기게임 종료]");
-                    	System.out.println("=====================");
+                    	System.out.println("===========================");
+                    	System.out.println("   [숫자맞추기게임 종료]");
+                    	System.out.println("===========================");
                     	break;
                     }
         		}
